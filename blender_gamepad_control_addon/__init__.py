582c75995877c385fad1a88da1dd3e41ab1f0c05
fabuloup <fabuloup@gmail.com>
2021-11-07 18:47:49 +0100

    Fixed addon not showing on search menu Fixed active camera control Added keyframes for position and rotation

--- blender_gamepad_control_addon/__init__.py
+++ blender_gamepad_control_addon/__init__.py
@@ -24,7 +24,10 @@ def worker():
 class GamepadControl(bpy.types.Operator):
     """Gamepad Control"""
     bl_idname = "view3d.gamepad_control"
-    bl_label = "Gamepad Control"
+    bl_label = "Gamepad_control"
+    bl_space_type = "View_3D"
+    bl_region_type = "UI"
+    bl_category = "Gamepad_control"
 
     _timer = None
     
@@ -32,6 +35,17 @@ class GamepadControl(bpy.types.Operator):
         'x':0,
         'y':0,
         'z':0
+    }
+
+    movement_speed = {
+        'x':0.1,
+        'y':0.1,
+        'z':0.1
+    }
+
+    rotation_speed = {
+        'x':1.0,
+        'y':0.5
     }
 
     def execute(self, context):
@@ -62,6 +76,8 @@ class GamepadControl(bpy.types.Operator):
         #catch gamepad input here
         global events
 
+        cam_obj = bpy.context.scene.camera
+
         if event.type in {'RIGHTMOUSE', 'ESC'}:
             self.cancel(context)
             return {'CANCELLED'}
@@ -76,45 +92,55 @@ class GamepadControl(bpy.types.Operator):
                         gpd_input = "Left Stick X"
                         if abs(gpd_value) < 3000 :
                             gpd_value = 0
-                        self.move['x'] = (float(gpd_value)/32768.0)*0.1
+                        self.move['x'] = (float(gpd_value)/32768.0)*self.movement_speed['x']
                     elif event.code == "ABS_Y":
                         gpd_input = "Left Stick Y"
                         if abs(gpd_value) < 3000 :
                             gpd_value = 0
-                        self.move['y'] = (float(gpd_value)/32768.0)*0.1
+                        self.move['y'] = (float(gpd_value)/32768.0)*self.movement_speed['y']
                     elif event.code == "ABS_RX":
                         gpd_input = "Right Stick X"
                         if abs(gpd_value) < 3000 :
                             gpd_value = 0
-                        self.move['rz'] = -(float(gpd_value)/32768.0)*0.3
+                        self.move['rz'] = -(float(gpd_value)/32768.0)*self.rotation_speed['x']
                     elif event.code == "ABS_RY":
                         gpd_input = "Right Stick Y"
                         if abs(gpd_value) < 3000 :
                             gpd_value = 0
-                        self.move['rx'] = -(float(gpd_value)/32768.0)*0.25
+                        self.move['rx'] = (float(gpd_value)/32768.0)*self.rotation_speed['y']
                     elif event.code == "ABS_Z":
                         gpd_input = "Left Trigger"
-                        self.move['z'] = -(float(gpd_value)/255.0)*0.1
+                        self.move['z'] = -(float(gpd_value)/255.0)*self.movement_speed['z']
                     elif event.code == "ABS_RZ":
                         gpd_input = "Right Trigger"
-                        self.move['z'] = (float(gpd_value)/255.0)*0.1
+                        self.move['z'] = (float(gpd_value)/255.0)*self.movement_speed['z']
                 events.pop(0)
 
-            bpy.data.objects['Camera'].rotation_mode = 'XYZ'
-            bpy.data.objects['Camera'].rotation_euler[0] += self.move['rx']*(math.pi / 180.0)
-            bpy.data.objects['Camera'].rotation_euler[2] += self.move['rz']*(math.pi / 180.0)
+            cam_obj.rotation_mode = 'XYZ'
+            cam_obj.rotation_euler[0] += self.move['rx']*(math.pi / 180.0)
+            cam_obj.rotation_euler[2] += self.move['rz']*(math.pi / 180.0)
 
-            theta = bpy.data.objects['Camera'].rotation_euler[2]
+            theta = cam_obj.rotation_euler[2]
 
-            bpy.data.objects['Camera'].location.x += self.move['x']*math.cos(theta)-self.move['y']*math.sin(theta)
-            bpy.data.objects['Camera'].location.y += self.move['x']*math.sin(theta)+self.move['y']*math.cos(theta)
-            bpy.data.objects['Camera'].location.z += self.move['z']
+            cam_obj.location.x += self.move['x']*math.cos(theta)-self.move['y']*math.sin(theta)
+            cam_obj.location.y += self.move['x']*math.sin(theta)+self.move['y']*math.cos(theta)
+            cam_obj.location.z += self.move['z']
+
+            cam_obj.keyframe_insert(data_path="location")
+            cam_obj.keyframe_insert(data_path="rotation_euler")
 
         return {'PASS_THROUGH'}
 
+def menu_func(self, context):
+    self.layout.operator(GamepadControl.bl_idname)
+
 def register():
     bpy.utils.register_class(GamepadControl)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
 
 
 def unregister():
     bpy.utils.unregister_class(GamepadControl)
+
+if __name__ == "__main__":
+    register()
